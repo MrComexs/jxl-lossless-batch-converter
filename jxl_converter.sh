da3c5884b4e1fd66cc099f1b78d819d9929971c6
comexs <mrcomexs@gmail.com>
2025-09-21 05:37:43 -0700

    added if statement to prevent file with the same name but different extention to overwrite eachother but multiple re-run of the script can defeat it

--- jxl_converter.sh
+++ jxl_converter.sh
@@ -31,13 +31,23 @@ fd -e jpg -e jpeg -e png | while IFS= read -r file; do
 
     if [[ "$file_path" == "$base_name" ]]; then
         sub_output_dir="$(echo "$file_path" | sed "s|$base_name||")"
-               
     else
          sub_output_dir="$(echo "$file_path" | sed "s|$selected_dir||")"       
     fi
 
+    if [ ! -d "$output_dir/$sub_output_dir" ]; then
     mkdir -p "$output_dir/$sub_output_dir"    
-    # What if two different types of files with the same name in the same sub dir 
-    cjxl "$file" -e 1 -d 0 "$output_dir/$sub_output_dir/${no_extension}.jxl"
+    fi
+
+    output_jxl="$output_dir/$sub_output_dir/${no_extension}.jxl"
+    # what if user ran the script again both $output_jxl and water-cats.jpg.jxl exist it would overwrite water-cats each time
+    # either check if $output_dir exist than exit or fix below
+    if [ -f "$output_jxl" ]; then
+        cjxl "$file" -e 1 -d 0 "$output_dir/$sub_output_dir/${base_name}.jxl"
+    else
+        cjxl "$file" -e 1 -d 0 "$output_jxl"
+    fi
     echo
+    # add zenity prompt to ask user if they want to remove orginal file
+    # rm "$file" 
 done
