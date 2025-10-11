b592393f675fd0c60473e26f50248f12f144b871
comexs <mrcomexs@gmail.com>
2025-10-10 22:24:15 -0700

    add some checks if output file already exist

--- jxl_converter.sh
+++ jxl_converter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for cmd in zenity cjxl fd echo sed mkdir; do
+for cmd in zenity cjxl djxl jxlinfo fd echo sed mkdir mktemp mv identify; do
   if ! command -v "$cmd" >/dev/null 2>&1; then
     [[ $cmd == "zenity" ]] || zenity --error --text="Error: $cmd not found. Please install it first."
     exit 1
@@ -22,7 +22,8 @@ clear
 effort=$(zenity --entry --text "What effort would you like? \n \n The Higher the number the more effort cjxl spends \n \n 7 is the default for cjxl" --entry-text "1-10" --title "Enter Effort")
 
 if [[ $effort =~ ^[1-9]$|^10$ ]]; then
-    zenity --info --text="You entered: $effort"
+    :
+    #zenity --info --text="You entered: $effort"
 else
     zenity --error --text="Invalid input. Please enter a number between 1 and 10."
     exit 1
@@ -38,9 +39,27 @@ fi
 cd "$selected_dir"||exit 1
 
 output_dir="${selected_dir}/converted_jxl"
-mkdir "$output_dir"
+
+if [ ! -d "$output_dir" ]; then
+    mkdir "$output_dir"
+ fi
+
+temp_dir=$(mktemp -d)
 
 fd -e jpg -e jpeg -e png | while IFS= read -r file; do
+
+    file_type=$(identify -format "%m" "$file")
+
+    if [ "$file_type" == JPEG ]; then
+        jpeg_cjxl_arg="-j 0"
+        echo "$file is a JPEG"
+    elif [ "$file_type" == PNG ]; then
+        echo "$file is a PNG"
+    else 
+        echo "$file isn't either a JPEG, or PNG. exiting"
+        exit 1
+    fi
+    
     base_name="${file##*/}"
     no_extension="${base_name%.*}"
     file_path="${file%/*}"
@@ -48,22 +67,55 @@ fd -e jpg -e jpeg -e png | while IFS= read -r file; do
     if [[ "$file_path" == "$base_name" ]]; then
         sub_output_dir="$(echo "$file_path" | sed "s|$base_name||")"
     else
-         sub_output_dir="$(echo "$file_path" | sed "s|$selected_dir||")"       
+        sub_output_dir="$(echo "$file_path" | sed "s|$selected_dir||")"       
     fi
 
     if [ ! -d "$output_dir/$sub_output_dir" ]; then
     mkdir -p "$output_dir/$sub_output_dir"    
     fi
 
+    temp_output_jxl="$temp_dir/${no_extension}.jxl"
     output_jxl="$output_dir/$sub_output_dir/${no_extension}.jxl"
-    # what if user ran the script again both $output_jxl and water-cats.jpg.jxl exist it would overwrite water-cats each time
     # either check if $output_dir exist than exit or fix below
+
+    cjxl "$file" -e "$effort" -d 0 $jpeg_cjxl_arg --quiet "$temp_output_jxl"
+
+    # Checks if "JPEG bitstream reconstruction data available" was outputed from the old jxl file
     if [ -f "$output_jxl" ]; then
-        cjxl "$file" -e "$effort" -d 0 "$output_dir/$sub_output_dir/${base_name}.jxl"
+        jxlinfo_output=$(jxlinfo "$output_jxl")
+            if echo "$jxlinfo_output" | grep -q "JPEG bitstream reconstruction data available"; then
+                djxl "$output_jxl" "$temp_dir/${no_extension}.jpg"
+                djxl_output_value=$(identify -format "%#\n" "$temp_dir/${no_extension}.jpg")
+                rm "$temp_dir/${no_extension}.jpg"
+            fi
+   fi
+
+    input_value=$(identify -format "%#\n" "$file")
+    temp_output_value=$(identify -format "%#\n" "$temp_output_jxl")
+    old_output_value=$(identify -format "%#\n" "$output_jxl")
+     
+    if [ ! -f "$output_jxl" ]; then
+        mv "$temp_output_jxl" "$output_jxl"
+    elif [ "$input_value" == "$old_output_value" ]; then
+        rm "$temp_output_jxl"
+        echo "$file old jxl match, not writing"
+        :
+    elif [ "$temp_output_value" == "$old_output_value" ]; then
+        rm "$temp_output_jxl"
+        echo "$file cjxl output match with old jxl"
+        :
+    elif [ "$input_value" == "$djxl_output_value" ]; then
+        rm "$temp_output_jxl"
+        echo "$file djxl value matched"
+        :
+    elif [ ! -f "$output_dir/$sub_output_dir/${base_name}.jxl" ];then
+        mv "$temp_output_jxl" "$output_dir/$sub_output_dir/${base_name}.jxl"
     else
-        cjxl "$file" -e "$effort" -d 0 "$output_jxl"
+        zenity --error --text="$file couldn't be written \n  \n${no_extension}.jxl and ${file}.jxl already exist"
+        rm "$temp_output_jxl"
     fi
     echo
-    # add zenity prompt to ask user if they want to remove orginal file
-    # rm "$file" 
 done
+
+rmdir "$temp_dir"
+rm -d "$output_dir" 2>/dev/null
